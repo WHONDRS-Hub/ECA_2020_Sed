cde8c1491c9bf25890847f7c595d80ae3ed61d04
stegen <James.Stegen@pnnl.gov>
2021-06-09 12:50:30 -0700

    Add files via upload

---
+++ ECA_Sed_FTICR_BetaDisp.r
@@ -0,0 +1,4 @@
+# using this to select samples for NMR and GC-MS
+# count number of samples in each site (wanting sites with 10 samples)
+# quantify beta dispersion with goal of selecting sites spanning a range
+
