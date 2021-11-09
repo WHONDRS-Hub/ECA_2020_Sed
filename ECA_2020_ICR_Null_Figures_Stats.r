29ea80998a02bbe16333b1d7a5c706bf34d59a68
stegen <James.Stegen@pnnl.gov>
2021-11-08 18:31:37 -0800

    Update ECA_2020_ICR_Null_Figures_Stats.r

--- ECA_2020_ICR_Null_Figures_Stats.r
+++ ECA_2020_ICR_Null_Figures_Stats.r
@@ -1,3 +1,13 @@
+rm(list=ls())
+
+library(crayon)
+
+background.color = "#d9d9d9" #to set background of plots
+data.color = "#abd9e9" # to set color for data like bars in histogram
+sig.lines.color = "#d73027" # to set color for vertical lines in plots
+kern.den.color = "#fee090"  # to set color for kernal density lines
+reg.dat.color = "#4575b4" # to set color for points in regressions
+
 # combine data to generate figures for manuscript on MCD null modeling of FTICR data from ECA 2020 campaign
 
 min.num.samples = 10
@@ -11,6 +21,10 @@ bnti.files = list.files(path = bnti.in.path,pattern = "MCD_bNTI_999")
 # compile bnti values across all sites to enable a density function
 
 bnti.comp = numeric()
+bnti.min.site = -999
+bnti.max.site = -999
+bnti.min.median = 999
+bnti.max.median = -999
 
 for (i in bnti.files) {
   
@@ -20,6 +34,25 @@ for (i in bnti.files) {
     
     bnti.comp = c(bnti.comp,as.numeric(as.vector(as.dist(bnti.temp))))
     
+    if (median(as.numeric(as.vector(as.dist(bnti.temp)))) < bnti.min.median ) {
+      
+      bnti.min.median = median(as.numeric(as.vector(as.dist(bnti.temp))))
+      bnti.min.site = i
+      
+      print(c('min',i,bnti.min.median))
+      
+    }
+    
+    if (median(as.numeric(as.vector(as.dist(bnti.temp)))) > bnti.max.median ) {
+      
+      bnti.max.median = median(as.numeric(as.vector(as.dist(bnti.temp))))
+      bnti.max.site = i
+      
+      print(c('max',i,bnti.max.median))
+      
+    }
+    
+    
   } else {
     
     print(c(nrow(bnti.temp),i))
@@ -28,11 +61,11 @@ for (i in bnti.files) {
   
 }
 
-bnti.comp.den = density(x = bnti.comp)
+bnti.comp.den = density(x = bnti.comp,from = min(bnti.comp),to = max(bnti.comp))
 
 # make one histogram for each site with the among site compiliation overlaid
 
-pdf(paste0(out.dir,"All_Sites_bNTI_Hists.pdf"),width=8)
+pdf(paste0(out.dir,"SI_All_Sites_bNTI_Hists.pdf"),width=8)
 
 for (i in bnti.files) {
   
@@ -41,10 +74,14 @@ for (i in bnti.files) {
   if (nrow(bnti.temp) >= min.num.samples) {
     
     par(pty="s")
-    hist(as.numeric(as.vector(as.dist(bnti.temp))),xlim=c(-2.5,max(bnti.comp)),cex.lab=2,cex.axis=1.5,main=paste0("Site ",substr(x = i,start = 8,stop = 9)),xlab=expression(paste(beta,"NTI")),ylab="Within-Site Observations")
-    abline(v=c(-2,2),col=2,lty=2,lwd=2)
+    hist(as.numeric(as.vector(as.dist(bnti.temp))),xlim=c(floor(min(bnti.comp)),ceiling(max(bnti.comp))),cex.lab=2,cex.axis=1.5,main=paste0("Site ",substr(x = i,start = 8,stop = 9)),xlab=expression(paste(beta,"NTI")),ylab="Within-Site Observations")
+    abline(v=floor(min(bnti.comp)),lwd=8000,col=background.color)
+    par(new=T,pty="s")
+    hist(as.numeric(as.vector(as.dist(bnti.temp))),xlim=c(floor(min(bnti.comp)),ceiling(max(bnti.comp))),cex.lab=2,cex.axis=1.5,main=paste0("Site ",substr(x = i,start = 8,stop = 9)),xlab=expression(paste(beta,"NTI")),ylab="Within-Site Observations",col=data.color)
+    abline(v=c(-2,2),lwd=2,col=sig.lines.color,lty=2)
+    axis(side = 1, at = c(floor(min(bnti.comp)),ceiling(max(bnti.comp))),cex.axis=1.5)
     par(new = TRUE) 
-    plot(bnti.comp.den,axes=F,lwd=2,col=4,xlab="",ylab="",main="")
+    plot(bnti.comp.den,axes=F,lwd=3,col=kern.den.color,xlab="",ylab="",main="")
     box()
     axis(side = 4, at = pretty(range(bnti.comp.den$y)),cex.axis=1.5)      # Add second axis
     mtext("Among Site Density", side = 4, line = 3,cex=2) 
@@ -56,7 +93,7 @@ for (i in bnti.files) {
 dev.off()
 
 # make a plot with two panels for the main ms. 
-# each panel has a histogram for the site with minimum bnti and maximum bnit (9 and 55)
+# each panel has a histogram for the site with minimum bnti and maximum bnit (9 and 41)
 # label a and b
 # overlay the among site density
 
@@ -68,12 +105,17 @@ for (i in bnti.files) {
   
   bnti.temp = read.csv(file = paste0(bnti.in.path,i),row.names = 1) 
   
-  if (nrow(bnti.temp) >= min.num.samples & substr(x = i,start = 8,stop = 9) %in% c('09','55')) {
+  if (nrow(bnti.temp) >= min.num.samples & substr(x = i,start = 8,stop = 9) %in% c('09','41')) {
     
-    hist(as.numeric(as.vector(as.dist(bnti.temp))),xlim=c(-2.5,max(bnti.comp)),cex.lab=2,cex.axis=1.5,main="",xlab=expression(paste(beta,"NTI")),ylab="Within-Site Observations")
-    abline(v=c(-2,2),col=2,lty=2,lwd=2)
+    cat(red("Note that the min and max median sites are hard coded here, check to make sure the correct sites are called"))
+    hist(as.numeric(as.vector(as.dist(bnti.temp))),xlim=c(floor(min(bnti.comp)),ceiling(max(bnti.comp))),cex.lab=2,cex.axis=1.5,main="",xlab=expression(paste(beta,"NTI")),ylab="Within-Site Observations")
+    abline(v=floor(min(bnti.comp)),lwd=8000,col=background.color)
+    par(new=T,pty="s")
+    hist(as.numeric(as.vector(as.dist(bnti.temp))),xlim=c(floor(min(bnti.comp)),ceiling(max(bnti.comp))),cex.lab=2,cex.axis=1.5,main=paste0("Site ",substr(x = i,start = 8,stop = 9)),xlab=expression(paste(beta,"NTI")),ylab="Within-Site Observations",col=data.color)
+    abline(v=c(-2,2),lwd=2,col=sig.lines.color,lty=2)
+    axis(side = 1, at = c(floor(min(bnti.comp)),ceiling(max(bnti.comp))),cex.axis=1.5)
     par(new = TRUE) 
-    plot(bnti.comp.den,axes=F,lwd=2,col=4,xlab="",ylab="",main="")
+    plot(bnti.comp.den,axes=F,lwd=3,col=kern.den.color,xlab="",ylab="",main="")
     box()
     axis(side = 4, at = pretty(range(bnti.comp.den$y)),cex.axis=1.5)      # Add second axis
     mtext("Among Site Density", side = 4, line = 3,cex=2) 
@@ -89,14 +131,14 @@ dev.off()
 
 
 # read in beta dispersion
-beta.disp = read.csv("//PNL/Projects/ECA_Project/ECA_Sediment_Extraction_ICR_Data/BetaDisp/ECA_BetaDisp_10_Samp_Only.csv",stringsAsFactors = F)
+beta.disp = read.csv("//PNL/Projects/ECA_Project/ECA_Sediment_Extraction_ICR_Data/BetaDisp/ECA2_FTICR_BetaDisp.csv",stringsAsFactors = F)
 head(beta.disp)
 str(beta.disp)
 
 # read in npoc and TN
-npoc = read.csv("//PNL/Projects/ECA_Project/Sediment_Collection_2020/Sediment_NPOC_TN/NPOC_TN_Direct_from_Instrument/ECA_NPOC.TN_Summary_Direct_from_Instrument.csv",stringsAsFactors = F)
-head(npoc)
-str(npoc)
+#npoc = read.csv("//PNL/Projects/ECA_Project/Sediment_Collection_2020/Sediment_NPOC_TN/NPOC_TN_Direct_from_Instrument/ECA_NPOC.TN_Summary_Direct_from_Instrument.csv",stringsAsFactors = F)
+#head(npoc)
+#str(npoc)
 
 # read texture
 texture = read.csv("//PNL/Projects/ECA_Project/Sediment_Collection_2020/Sediment_Texture/VGC_texture.csv",stringsAsFactors = F)
@@ -140,7 +182,7 @@ str(moist.sum)
 
 # merge everything
 merged.data = merge(bnti,texture,by = 'Site',all = T)
-merged.data = merge(merged.data,npoc,by = 'Site',all = T)
+#merged.data = merge(merged.data,npoc,by = 'Site',all = T)
 merged.data = merge(merged.data,beta.disp,by = 'Site',all = T)
 merged.data = merge(merged.data,moist.sum,by = 'Site',all = T)
 dim(merged.data)
@@ -151,10 +193,9 @@ bnti.moist = bnti.moist[which(bnti.moist$Sample_Number >= min.num.samples),]
 dim(bnti.moist)
 
 # start trying constraint based regression
-#!! need to run through a loop of different number of break points and record the p.val and r.sq for each and make a plot of those parameters vs. number of break points
 num.of.breaks = 10
 
-pdf(paste(out.dir,"MCD_Med_bNTI_v_Both_Moisture.pdf",sep=""),width=15)
+pdf(paste(out.dir,"Main_Med_bNTI_v_Both_Moisture.pdf",sep=""),width=15)
 par(pty="s",mfrow=c(1,2))
 
 # do moisture on dry basis first
@@ -202,14 +243,14 @@ dim(parsed.bnti)
 
 plot((bnti.moist$Median_bNTI) ~ (bnti.moist[,moist.var]),ylab=expression(paste("Site-Level Median ",beta,"NTI",sep="")),xlab="Site-Level Median Moisture (per dry mass)",cex.lab=2,cex.axis=1.5)
 mod.to.plot = parsed.bnti$max.bnti ~ parsed.bnti$moisture
-points(mod.to.plot,pch=19,col=4)
+points(mod.to.plot,pch=19,col=reg.dat.color)
 mod.lm = summary(lm(mod.to.plot))
-abline(mod.lm,lwd=2,col=4)
+abline(mod.lm,lwd=2,col=reg.dat.color)
 p.val = round(mod.lm$coefficients[2,4],digits = 2)
 r.sq = round(mod.lm$r.squared,digits = 2)
 mtext(text = paste("p = ",p.val," ",sep=""),line = -1.5,adj = 1,side = 3,cex=1.5)
 mtext(text = substitute(paste(R^2," = ", r.sq," "), list(r.sq=r.sq)),line = -3,adj = 1,side = 3,cex=1.5)
-mtext(text = " a",side = 3,line = -1.5,adj = 0,cex=2)
+mtext(text = "   a",side = 3,line = -1.5,adj = 0,cex=2)
 
 
 # do moisture on wet basis
@@ -257,9 +298,9 @@ dim(parsed.bnti)
 
 plot((bnti.moist$Median_bNTI) ~ (bnti.moist[,moist.var]),ylab=expression(paste("Site-Level Median ",beta,"NTI",sep="")),xlab="Site-Level Median Moisture (per wet mass)",cex.lab=2,cex.axis=1.5)
 mod.to.plot = parsed.bnti$max.bnti ~ parsed.bnti$moisture
-points(mod.to.plot,pch=19,col=4)
+points(mod.to.plot,pch=19,col=reg.dat.color)
 mod.lm = summary(lm(mod.to.plot))
-#abline(mod.lm,lwd=2,col=4)
+#abline(mod.lm,lwd=2,col=reg.dat.color)
 p.val = round(mod.lm$coefficients[2,4],digits = 2)
 r.sq = round(mod.lm$r.squared,digits = 2)
 mtext(text = paste("p = ",p.val," ",sep=""),line = -1.5,adj = 1,side = 3,cex=1.5)
@@ -268,16 +309,23 @@ mtext(text = " b",side = 3,line = -1.5,adj = 0,cex=2)
 
 dev.off()
 
-!! next step is making the histogram of the median values, code just below, just need to make sure it's good
-
-
-# make bnti histogram
-pdf("//PNL/Projects/ECA_Project/ECA_Sediment_Extraction_ICR_Data/Null_Modeling/MCD_bNTI_Outcomes/bNTI_MCD_Histogram.pdf")
-par(pty="s")
-hist(merged.data$Median_bNTI,xlim=c(-2.5,39),xlab=expression(paste("Within Site Median ",beta,"NTI",sep="")),cex.lab=2,cex.axis=1.5,main="")
-abline(v=c(-2,2),lwd=2,col=2)
+######
+# make histogram of bnti median values
+pdf(paste(out.dir,"Main_Hist_of_Med_bNTI.pdf",sep=""),width = 15)
+  par(pty="s",mfrow=c(1,2))
+  hist(merged.data$Median_bNTI,xlim=c(floor(min(bnti.comp)),ceiling(max(bnti.comp))),xlab=expression(paste("Within Site Median ",beta,"NTI",sep="")),cex.lab=2,cex.axis=1.5,main="")
+  abline(v=floor(min(bnti.comp)),lwd=8000,col=background.color)
+  par(new=T,pty="s")
+  hist(merged.data$Median_bNTI,xlim=c(floor(min(bnti.comp)),ceiling(max(bnti.comp))),xlab=expression(paste("Within Site Median ",beta,"NTI",sep="")),cex.lab=2,cex.axis=1.5,main="",col = data.color)
+  abline(v=c(-2,2),lwd=2,col=sig.lines.color,lty=2)
+  axis(side = 1, at = c(floor(min(bnti.comp)),ceiling(max(bnti.comp))),cex.axis=1.5)
+  box()
 dev.off()
+#######
 
+
+#############################
+haven't edited below here
 
 # selecting NMR samples. going for sets with high NPOC, but without losing main range in bNTI
 # median of 40 didn't drop the overall range in bNTI
